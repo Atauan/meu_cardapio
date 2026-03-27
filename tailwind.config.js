6250573e92e339d440b93db6a323658cddbce9d7
Atauan <94688135+Atauan@users.noreply.github.com>
2026-03-27 01:09:22 -0300

    update vercel compatility

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./meu_cardapio.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
